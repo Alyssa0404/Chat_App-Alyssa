dff505592eafbdcf2cc554252683f02cb3c6b32f
AlyssaBalaoro <balaoroalyssa@gmail.com>
2022-12-29 20:44:35 +0800

    styling css

--- index.js
+++ index.js
@@ -18,6 +18,35 @@ io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
   
+    socket.on('join', ({ username, room }, callback) => {
+        const { error, user } = addUser({ id: socket.id, username, room })
+        if (error) return callback(error)
+        socket.join(user.room)
+        socket.emit('message', generateMsg('Administrator','Welcome to the chat app!'))
+        socket.broadcast.to(user.room).emit('message', generateMsg('Administrator',`${user.username} has joined!`))
+        io.to(user.room).emit('dataRoom', {
+            room: user.room,
+            users: getUsersInRoom(user.room)
+        })
+        callback()
+    })
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id)
+        io.to(user.room).emit('message', generateMsg(user.username, message))
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id)
+        if (user) {
+            io.to(user.room).emit('message', generateMsg(user.username, `${user.username} has left!`))
+            io.to(user.room).emit('dataRoom', {
+                room: user.room,
+                users: getUsersInRoom(user.room)
+            })
+        }
+    })
 })
 
 server.listen(port, () => console.log(`Server is up on port ${port}!`))
--- public/main.js
+++ public/main.js
@@ -17,4 +17,35 @@ socket.on('message', (message) => {
     })
     msg.insertAdjacentHTML('beforeend', html)
 })
+socket.on('dataRoom', ({ room, users }) => {
+    const html = Mustache.render(sidebarTemplate, {
+        room,
+        users
+    })
+    document.querySelector('.sidebar').innerHTML = html
+})
+
+msgForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    msgFormBtn.setAttribute('disabled', 'disabled')
+    const message = msgFormInput.value
+
+    socket.emit('sendMessage', message, error => {
+        msgFormBtn.removeAttribute('disabled')
+        msgFormInput.value = ''
+        msgFormInput.focus()
+
+        if (error) {
+            return console.log(error)
+        }
+        console.log('Message delivered!')
+    })
+})
+
+socket.emit('join', { username, room }, (error) => {
+    if (error) {
+        alert(error)
+        location.href = '/'
+    }
+})
 
